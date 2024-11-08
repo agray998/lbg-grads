d24338700247e49ad2d149b548d839ec058e6d8c
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2024-11-08 13:36:42 +0000

    Update install-docker-and-compose.sh

--- week4/install-docker-and-compose.sh
+++ week4/install-docker-and-compose.sh
@@ -3,5 +3,5 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y curl jq git docker.io
 sudo gpasswd -a $(whoami) docker
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-sudo curl -L https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
